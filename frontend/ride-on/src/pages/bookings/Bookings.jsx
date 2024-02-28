2f1d6d3fb1531f812ca1f8254114738193741d22
Nate <131452730+np-armstrong@users.noreply.github.com>
2024-02-29 00:20:49 +0700

    Modal for editing bookings added
    Date validation working

--- frontend/ride-on/src/pages/bookings/Bookings.jsx
+++ frontend/ride-on/src/pages/bookings/Bookings.jsx
@@ -2,12 +2,14 @@ import { useState } from 'react'
 import Navigation from '../../components/Navigation/Navigation'
 import { Container, Image, Row, Col, Button } from 'react-bootstrap'
 import './bookings.css'
+import EditModal from '../../components/edit/Edit'
 
 function Bookings() {
 
   // This array will be used to store dummy data for the bookings until the backend is connected
   let myBookings = [
     {
+      'id': 1,
       'make': 'Honda',
       'model': 'CBR 1000RR',
       'start_date': '26/02/2024',
@@ -15,18 +17,20 @@ function Bookings() {
       'image': "https://frasermotorcycles.com.au/cdn/shop/products/HONDA_CBR1000RR-R_SP_2022_2_1200x.png?v=1651111375"
     },
     {
+      'id': 2,
       'make': 'Honda',
       'model': 'Zoomer X',
       'start_date': '26/02/2024',
       'end_date': '26/03/2024',
       'image': "https://www.ncxhonda.com/motorcycles/storage/app/uploads/360/zoomer-x/009.jpg"
-    }
+    },
   ]
 
   const [openEditModal, setOpenEditModal] = useState(false);
 
   //Handle Cancellation 
   function handleCancel(){
+
     console.log("Cancel Button Clicked");
   }
 
@@ -38,7 +42,7 @@ function Bookings() {
 
   return (
     <>
-      <Navigation isBookings={true}/> {/* changes the navebar option based on page */}
+      <Navigation isBookings={true}/> {/* changes the navbar option based on page */}
       <Container>
         <div className="header">        
           <h1>My Bookings</h1>
@@ -48,15 +52,9 @@ function Bookings() {
         <div>
           {myBookings.map((booking, index) => {
             return (
-              // <div key={index}>
-              //   <h5>{booking.make} {booking.model}</h5>
-              //   <p>Start Date: {booking.start_date}</p>
-              //   <p>End Date: {booking.end_date}</p>
-              // </div>
               <Container key={index} className='booking-container'>
                 <div className="booking">
                   <div className="img-container">
-                    {/* <Image width={100} height={100} src={booking.image} roundedCircle />  */}
                     <img src={booking.image} />
                   </div>
                   <div className="booking-details">
@@ -67,14 +65,22 @@ function Bookings() {
                   </div>
                   <div className="booking-controls">
                     <Button variant="outline-dark" onClick={handleEdit}>Edit</Button>
+                    {/* Open the edit modal */}
+                    {openEditModal && <EditModal 
+                      show={openEditModal} 
+                      onHide={() => setOpenEditModal(false)} 
+                      title={`Edit Booking id: ${booking.id}`}
+                      bike={`${booking.make} ${booking.model}`}
+                    />}
                     <Button variant="outline-danger" onClick={handleCancel}>Cancel</Button>
                   </div>
                 </div>    
               </Container>
             )
-          })}
+          })}      
         </div>
       </Container>
+
     </>
   )
 }
